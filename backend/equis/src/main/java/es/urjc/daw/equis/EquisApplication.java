3a0f64a0b60036c0c6c0fd1290d7f4bd01a0a6ac
Elena Ceinos <elenaceinos@hotmail.com>
2026-02-12 21:00:06 +0100

    created backend w/spring

---
+++ backend/equis/src/main/java/es/urjc/daw/equis/EquisApplication.java
@@ -0,0 +1,13 @@
+package es.urjc.daw.equis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquisApplication.class, args);
+	}
+
+}
---
+++ backend/equis/src/test/java/es/urjc/daw/equis/EquisApplicationTests.java
@@ -0,0 +1,13 @@
+package es.urjc.daw.equis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
