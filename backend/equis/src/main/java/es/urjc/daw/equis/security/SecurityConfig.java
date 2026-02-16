be05b165ff4c1d0cfbe4aa4f386233c9d611aa3e
Elena Ceinos <elenaceinos@hotmail.com>
2026-02-16 18:02:51 +0100

    fixed mustache, no user pass, encrypted pass, added dinamic profile

--- backend/equis/src/main/java/es/urjc/daw/equis/security/SecurityConfig.java
+++ backend/equis/src/main/java/es/urjc/daw/equis/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package es.urjc.daw.equis.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,8 +24,8 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
-                .loginPage("/login")           // GET /login -> tu mustache
-                .loginProcessingUrl("/login")  // POST /login -> lo procesa Spring Security
+                .loginPage("/login")           
+                .loginProcessingUrl("/login")  
                 .defaultSuccessUrl("/", true)
                 .failureUrl("/login?error")
                 .permitAll()
@@ -36,5 +38,9 @@ public class SecurityConfig {
 
         return http.build();
     }
-}
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
