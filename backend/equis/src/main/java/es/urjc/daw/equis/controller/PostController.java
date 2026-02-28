3e16baf0e8efb30284e0079deb22f85621903c05
Cassiel123 <4610@alumnoaltamira.es>
2026-02-28 16:48:17 +0100

    Creation of new posts, update of the index view to display posts with and without images

--- backend/equis/src/main/java/es/urjc/daw/equis/controller/PostController.java
+++ backend/equis/src/main/java/es/urjc/daw/equis/controller/PostController.java
@@ -1,12 +1,18 @@
 package es.urjc.daw.equis.controller;
 
+import java.sql.Blob;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import es.urjc.daw.equis.model.Comment;
 import es.urjc.daw.equis.model.Like;
@@ -16,6 +22,7 @@ import es.urjc.daw.equis.repository.CommentRepository;
 import es.urjc.daw.equis.repository.LikeRepository;
 import es.urjc.daw.equis.repository.PostRepository;
 import es.urjc.daw.equis.repository.UserRepository;
+import es.urjc.daw.equis.service.PostService;
 
 @Controller
 @RequestMapping("/posts")
@@ -25,7 +32,8 @@ public class PostController {
     private final LikeRepository likeRepository;
     private final UserRepository userRepository;
     private final CommentRepository commentRepository;
-
+    @Autowired
+    private PostService postService;
     public PostController(PostRepository postRepository,
                           LikeRepository likeRepository,
                           UserRepository userRepository,
@@ -34,6 +42,40 @@ public class PostController {
         this.likeRepository = likeRepository;
         this.userRepository = userRepository;
         this.commentRepository = commentRepository;
+    }
+
+
+    @PostMapping("/newPost")
+    public String newPost(@RequestParam String content,
+                    @RequestParam Long category_id,
+                    @RequestParam(required = false)  MultipartFile picture,
+                    Authentication auth) throws Exception{
+        
+        if (auth == null || auth.getName().equals("anonymousUser")) {
+            return "redirect:/login";
+        }
+        System.out.println("entra a guardar el post");
+        Post post = new Post();
+        postService.save(post, picture, auth, category_id,content);
+        System.out.println("sale del postservice");
+        return "redirect:/";
+    }
+    @GetMapping("/post/{id}/image")
+    public ResponseEntity<byte[]> getPostImage(@PathVariable Long id) throws Exception {
+
+        Post post = postRepository.findById(id).orElse(null);
+
+        if (post == null || post.getPicture() == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Blob blob = post.getPicture();
+        byte[] imageBytes = blob.getBytes(1, (int) blob.length());
+
+        return ResponseEntity
+                .ok()
+                .header("Content-Type", "image/jpeg")
+                .body(imageBytes);
     }
 
     @GetMapping("/{id}/like")
--- backend/equis/src/main/java/es/urjc/daw/equis/service/PostService.java
+++ backend/equis/src/main/java/es/urjc/daw/equis/service/PostService.java
@@ -1,17 +1,30 @@
 package es.urjc.daw.equis.service;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.sql.rowset.serial.SerialBlob;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.security.core.Authentication;
 
+import es.urjc.daw.equis.model.Category;
 import es.urjc.daw.equis.model.Post;
+import es.urjc.daw.equis.model.User;
 import es.urjc.daw.equis.repository.LikeRepository;
 import es.urjc.daw.equis.repository.PostRepository;
 
 @Service
 public class PostService {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     private final PostRepository postRepository;
     private final LikeRepository likeRepository;
@@ -29,9 +42,29 @@ public class PostService {
         }
     }
 
-    public Post save(Post post) {
-        return postRepository.save(post);
+    public void save(Post post, MultipartFile picture,Authentication auth, Long category_id, String content) throws IOException{
+        //asigno el contenido
+        post.setContent(content);
+        // asignacion del usuario correspondiente
+        User user = userService.findByEmail(auth.getName()).orElse(null);
+        post.setUser(user);
+
+        // asignacion de la categoria
+        Category cateory = categoryService.findById(category_id);
+        post.setCategory(cateory);
+        System.out.println("asigna todo menos foto");
+        //asignacion de la imagen si es que la hay
+        if (picture != null && !picture.isEmpty()) {
+        try {
+            post.setPicture(new SerialBlob(picture.getBytes()));
+        } catch (Exception e) {
+            throw new IOException("Failed to create image blob", e);
+        }
+        System.out.println("asigna foto");
+        }
+        postRepository.save(post);
     }
+
     public Page<Post> getPostsByUserId(Long userId, Pageable pageable) {
         return postRepository.findByUserIdOrderByDateDesc(userId, pageable);
     }
