bcc0c516ecff19f6c4f3655023f4e6c4893a9f78
David Paul Limaylla Ticlavilca <dp.limaylla.2021@alumnos.urjc.es>
2026-03-19 18:24:13 +0100

    cookies fixed / index.html added partials

--- backend/equis/src/main/java/es/urjc/daw/equis/security/RepositoryUserDetailsService.java
+++ backend/equis/src/main/java/es/urjc/daw/equis/security/RepositoryUserDetailsService.java
@@ -22,9 +22,10 @@ public class RepositoryUserDetailsService implements UserDetailsService {
 	private UserRepository userRepository;
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException {
 
-		User user = userRepository.findByName(username)
+		User user = userRepository.findByEmail(username)
 				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
 		List<GrantedAuthority> roles = new ArrayList<>();
@@ -32,7 +33,10 @@ public class RepositoryUserDetailsService implements UserDetailsService {
 			roles.add(new SimpleGrantedAuthority("ROLE_" + role));
 		}
 
-		return new org.springframework.security.core.userdetails.User(user.getName(), 
-				user.getEncodedPassword(), roles);
+		return new org.springframework.security.core.userdetails.User(
+				user.getEmail(),                
+				user.getEncodedPassword(),
+				roles
+		);
 	}
 }
